554a2a888742991d2e1f058cda43035b90d90d0c
Nat-crit20 <93047867+Nat-crit20@users.noreply.github.com>
2023-01-06 10:19:33 -0600

    Updated pokemonList array
    Added Pokémon to the empty objects.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,8 @@
 let pokemonList = [
-  { name: "", height: "", type: "" },
-  { name: "", height: "", type: "" },
-  { name: "", height: "", type: "" },
-  { name: "", height: "", type: "" },
-  { name: "", height: "", type: "" },
-  { name: "", height: "", type: "" },
+  { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
+  { name: "Beedrill", height: 1, type: ["bug", "poison"] },
+  { name: "Pidgey", height: 0.3, type: ["flying", "normal"] },
+  { name: "Groudon", height: 3.5, type: ["ground"] },
+  { name: "Zekrom", height: 2.9, type: ["dragon", "electric"] },
+  { name: "Meloetta", height: 0.6, type: ["psychic", "normal"] },
 ];
