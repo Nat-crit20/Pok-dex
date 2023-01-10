cc87630d5d99a04c0cf7ff979c24d0381ea93e49
Nat-crit20 <93047867+Nat-crit20@users.noreply.github.com>
2023-01-10 11:11:35 -0600

    Updated script.js with IIFE
    Moved pokemonList to pokemonRepository IIFE

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,11 +1,13 @@
-let pokemonList = [
-  { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
-  { name: "Beedrill", height: 1, type: ["bug", "poison"] },
-  { name: "Pidgey", height: 0.3, type: ["flying", "normal"] },
-  { name: "Groudon", height: 3.5, type: ["ground"] },
-  { name: "Zekrom", height: 2.9, type: ["dragon", "electric"] },
-  { name: "Meloetta", height: 0.6, type: ["psychic", "normal"] },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
+    { name: "Beedrill", height: 1, type: ["bug", "poison"] },
+    { name: "Pidgey", height: 0.3, type: ["flying", "normal"] },
+    { name: "Groudon", height: 3.5, type: ["ground"] },
+    { name: "Zekrom", height: 2.9, type: ["dragon", "electric"] },
+    { name: "Meloetta", height: 0.6, type: ["psychic", "normal"] },
+  ];
+})();
 
 pokemonList.forEach((pokemon) => {
   let comment = `${pokemon.name} (height: ${pokemon.height})`;
