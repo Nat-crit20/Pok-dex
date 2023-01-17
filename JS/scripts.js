c5fc8f5fafeae6aa0b864db35f407c03336d917e
Nat-crit20 <93047867+Nat-crit20@users.noreply.github.com>
2023-01-17 15:46:54 -0600

    Updated pokemonList in script.js
    Updated pokemonList array to be empty in script.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,36 +1,5 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: "Charizard",
-      height: 1.7,
-      type: ["fire", "flying"],
-    },
-    {
-      name: "Beedrill",
-      height: 1,
-      type: ["bug", "poison"],
-    },
-    {
-      name: "Pidgey",
-      height: 0.3,
-      type: ["flying", "normal"],
-    },
-    {
-      name: "Groudon",
-      height: 3.5,
-      type: ["ground"],
-    },
-    {
-      name: "Zekrom",
-      height: 2.9,
-      type: ["dragon", "electric"],
-    },
-    {
-      name: "Meloetta",
-      height: 0.6,
-      type: ["psychic", "normal"],
-    },
-  ];
+  let pokemonList = [];
 
   function getAll() {
     return pokemonList;
